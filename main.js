ceebb2d8f197e41c193fae9054f7743a1c227dac
chentai-tech <127871196+chentai-tech@users.noreply.github.com>
2024-03-10 20:03:46 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 var script_url = "https://script.google.com/macros/s/AKfycbwmGJwNfWqzS-OABXY7V7M9WiyPyscC87HGx1ZUDUIkj74uUbPncCWiVEzmcfVTOeoyXA/exec";
-(function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "leh6b3a7z6");
 $(document).ready(function ($) {
     $("#google-sheet").submit(function (event) {
         //$('#date, #time, #name, #option').prop("disabled", false);
